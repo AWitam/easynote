a853ebea33c641ed97bc751bd63e093c54fb93b6
wiktoria7001 <93009130+wiktoria7001@users.noreply.github.com>
2023-05-23 17:42:44 +0200

    Upgrade dependencies

--- app/src/main/java/com/example/easynote/ui/notes/AddNote.kt
+++ app/src/main/java/com/example/easynote/ui/notes/AddNote.kt
@@ -2,11 +2,20 @@ package com.example.easynote.ui.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import com.example.easynote.R
 
 class AddNote : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+
+        val titleInput: EditText = findViewById(R.id.notes_title)
+        val descriptionInput: EditText = findViewById(R.id.notes_description)
+        val saveBtn: ImageView = findViewById(R.id.icon_save)
+
+
     }
 }
