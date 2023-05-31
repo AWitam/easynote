0a724af07a25bbf126c6e468dd91c9768335d7fd
wiktoria7001 <93009130+wiktoria7001@users.noreply.github.com>
2023-05-31 14:03:36 +0200

    Setup Room DB

--- app/src/main/java/com/example/easynote/models/Note.kt
+++ app/src/main/java/com/example/easynote/models/Note.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes_table")
 data class Note(
-    @PrimaryKey(autoGenerate= true) val id: Int?,
-    @ColumnInfo(name = "title") val title: String?,
-    @ColumnInfo (name = "note") val note: String?,
-    @ColumnInfo(name = "date") val date: String?
+    @PrimaryKey(autoGenerate= true) var id: Int?,
+    @ColumnInfo(name = "title") var title: String?,
+    @ColumnInfo (name = "note") var note: String?,
+    @ColumnInfo(name = "date") var date: String?
     ) : java.io.Serializable
